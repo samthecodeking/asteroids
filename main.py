2d61abe1235827fca077270ccc18c5be1db60895
samthecodeking <sam.leigh1987@gmail.com>
2024-11-08 11:01:39 +0000

    Added player rotation

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
@@ -21,7 +20,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen) 
+        player.draw(screen)
+        player.update(dt) 
         pygame.display.flip()  # display update
         dt = clock.tick(60) /1000 # Limit framerate to 60 FPS
 
